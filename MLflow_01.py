12cc5bc04111bf2dc2c9995b5c918019ef465d3f
Hmzkhnswt <hamzakhanswati117191@gmail.com>
2024-07-12 23:37:49 +0500

    runs with winequality data

--- MLflow_01.py
+++ MLflow_01.py
@@ -1,66 +1,99 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
-from tensorflow.keras import Model
+# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
 import mlflow
-import mlflow.tensorflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
 
-# Set the tracking URI to local file system
-mlflow.set_tracking_uri("file:///tmp/mlruns")
-mlflow.set_experiment("TensorFlow Demo")
+import logging
 
-# Load the MNIST dataset
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
-# Normalize the data
-x_train, x_test = x_train / 255.0, x_test / 255.0
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
-# Add a channel dimension to the data
-x_train = x_train[..., tf.newaxis]
-x_test = x_test[..., tf.newaxis]
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-# Create a simple neural network model
-class MyModel(Model):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation='relu')
-        self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
-        self.d2 = Dense(10, activation='softmax')
+    # Read the wine-quality csv file from the URL
+    csv_url = (
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
 
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.flatten(x)
-        x = self.d1(x)
-        return self.d2(x)
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
 
-model = MyModel()
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-# Define the training process within an MLflow run
-with mlflow.start_run():
-    # Log model summary
-    model.summary()
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
 
-    # Log hyperparameters
-    mlflow.log_param("optimizer", "adam")
-    mlflow.log_param("loss_function", "sparse_categorical_crossentropy")
-    mlflow.log_param("batch_size", 32)
-    mlflow.log_param("epochs", 5)
+        predicted_qualities = lr.predict(test_x)
 
-    # Train the model
-    history = model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    # Log metrics
-    for epoch in range(5):
-        mlflow.log_metric("train_loss", history.history['loss'][epoch], step=epoch)
-        mlflow.log_metric("train_accuracy", history.history['accuracy'][epoch], step=epoch)
-        mlflow.log_metric("val_loss", history.history['val_loss'][epoch], step=epoch)
-        mlflow.log_metric("val_accuracy", history.history['val_accuracy'][epoch], step=epoch)
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
 
-    # Log the model
-    mlflow.tensorflow.log_model(model, "model")
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        
+        # # For remote server only (Dagshub)
+        # remote_server_uri = "https://dagshub.com/entbappy/MLflow-Basic-Demo.mlflow"
+        # mlflow.set_tracking_uri(remote_server_uri)
+
+
+        # For remote server only (AWS)
+        remote_server_uri = "http://ec2-54-147-36-34.compute-1.amazonaws.com:5000/"
+        mlflow.set_tracking_uri(remote_server_uri)
+
+
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
---
+++ experiments.py
@@ -0,0 +1,4 @@
+import sys
+
+arg = sys.argv
+print("Setting the arguments to run the code: -------->",arg[1],"<--------")
